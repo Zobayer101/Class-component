f1add1556ee29830e2d829e7f22e577a861e7ffb
Zobayer <mdzobayerhossen520@gmail.com>
2024-02-17 21:46:34 +0600

    i am commit

--- Class11/src/App.jsx
+++ Class11/src/App.jsx
@@ -1,16 +1,13 @@
 
 
-import ClockList from './clsCom/ClockList';
+
 
 function App() {
-//Lists and Keys
-  const arr=[3,4,2,5,65,3]
+  //useState
 
   return (
     <>
-  
-     <ClockList quantity={arr}></ClockList>
-     
+    <h1>hello bangla</h1>
     </>
   )
 }
